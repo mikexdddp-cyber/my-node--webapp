d4d6d8aa3d4f39b7efdef19d1b542eb8c0d787f1
mikexdddp-cyber <_github@gmail.com>
2026-08-19 12:16:46 +0700

    fix db connection port and ssl

--- config/db.js
+++ config/db.js
@@ -4,9 +4,13 @@ require('dotenv').config();
 // Connection Pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
